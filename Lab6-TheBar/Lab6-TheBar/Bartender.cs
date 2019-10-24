aa3fa59372689d42b531f54fe1ed76dde2f45f4b
Nils Lundell <nils.solumfaeste@gmail.com>
2019-10-24 14:05:53 +0200

    Added logic structure to Bartender

--- Lab6-TheBar/Lab6-TheBar/Bartender.cs
+++ Lab6-TheBar/Lab6-TheBar/Bartender.cs
@@ -1,4 +1,8 @@
-﻿namespace Lab6_TheBar
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab6_TheBar
 {
     internal class Bartender
     {
@@ -9,6 +13,32 @@
         public Bartender(Bar bar)
         {
             this.bar = bar;
+            Task.Run(() => 
+            {
+                while(bar.isOpen)
+                {
+                    WaitForPatron();
+                    Thread.Sleep(3000);
+                    WaitForGlass();
+                    Thread.Sleep(3000);
+                    ServePatron();
+                }
+            });
+        }
+
+        private void ServePatron()
+        {
+            //TODO: Serve Patron
+        }
+
+        private void WaitForGlass()
+        {
+            //TODO: Wait for glass
+        }
+
+        private void WaitForPatron()
+        {
+            // TODO: Wait for patron
         }
     }
 }
