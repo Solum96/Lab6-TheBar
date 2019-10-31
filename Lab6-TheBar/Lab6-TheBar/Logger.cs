f8fe7db936027478cf823f3a82a1a7883149367c
Nils Lundell <nils.solumfaeste@gmail.com>
2019-10-31 11:47:31 +0100

    created a Logger class

---
+++ Lab6-TheBar/Lab6-TheBar/Logger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Lab6_TheBar
+{
+    internal class Logger
+    {
+        MainWindow mainWindow;
+
+        public static ObservableCollection<string> bartenderList;
+        public static ObservableCollection<string> waiterList;
+        public static ObservableCollection<string> patronList;
+
+        public Logger(MainWindow mainWindow)
+        {
+            this.mainWindow = mainWindow;
+            mainWindow.bartenderLog.ItemsSource = bartenderList;
+            mainWindow.waiterLog.ItemsSource = waiterList;
+            mainWindow.patronLog.ItemsSource = patronList;
+        }
+
+    }
+}
--- Lab6-TheBar/Lab6-TheBar/MainWindow.xaml.cs
+++ Lab6-TheBar/Lab6-TheBar/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,10 +22,13 @@ namespace Lab6_TheBar
     public partial class MainWindow : Window
     {
         Bar bar;
+        Logger log;
         public MainWindow()
         {
             InitializeComponent();
             bar = new Bar(this);
+            log = new Logger(this);
+
             runButton.Click += RunButtonClick;
         }
 
